74b4b65ff3a357b540b9f8a6efae094a542893b2
Artem Koton <kotonartem@protonmail.com>
2025-03-02 23:26:18 +0300

    Don't emit error on non-existent version file

--- upgrade.sh
+++ upgrade.sh
@@ -2,12 +2,12 @@
 
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-CURRENT_VERSION=$(cat $SCRIPT_DIR/.runner-version)
+CURRENT_VERSION_FILE=$SCRIPT_DIR/.runner-version
 NEW_VERSION=$(curl -s "https://api.github.com/repos/actions/runner/releases/latest" | jq --raw-output '.tag_name' | tr -d v)
 
 COMPILERS=( gcc-14 clang-19 )
 
-if [[ "$NEW_VERSION" != "$CURRENT_VERSION" ]]; then
+if [[ ! -f "$CURRENT_VERSION_FILE" || "$NEW_VERSION" != $(cat "$CURRENT_VERSION_FILE") ]]; then
     for compiler in "${COMPILERS[@]}"; do
         full_version="$NEW_VERSION-${compiler}"
         docker build -t cpp-kt/gh-runner-ubuntu:$full_version --build-arg "COMPILER=$compiler" --build-arg VERSION=$NEW_VERSION $SCRIPT_DIR
