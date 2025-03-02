31a7185d17f1d75a36381b9cdebfdd16229209f6
Artem Koton <kotonartem@protonmail.com>
2025-03-02 23:04:07 +0300

    Rename runner-version to .runner-version

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-CURRENT_VERSION=$(cat $SCRIPT_DIR/runner-version)
+CURRENT_VERSION=$(cat $SCRIPT_DIR/.runner-version)
 NEW_VERSION=$(curl -s "https://api.github.com/repos/actions/runner/releases/latest" | jq --raw-output '.tag_name' | tr -d v)
 
 COMPILERS=( gcc-14 clang-19 )
@@ -13,7 +13,7 @@ if [[ "$NEW_VERSION" != "$CURRENT_VERSION" ]]; then
         docker build -t cpp-kt/gh-runner-ubuntu:$full_version --build-arg "COMPILER=$compiler" --build-arg VERSION=$NEW_VERSION $SCRIPT_DIR
     done
 
-    echo $NEW_VERSION > $SCRIPT_DIR/runner-version
+    echo $NEW_VERSION > $SCRIPT_DIR/.runner-version
 else
     echo "No upgrade needed."
 fi
