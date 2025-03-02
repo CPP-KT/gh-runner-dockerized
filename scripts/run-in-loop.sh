8c0ded75be0edee5b55b182390351dc514210048
Artem Koton <kotonartem@protonmail.com>
2025-03-03 02:41:20 +0300

    Improve runner labels

--- scripts/run-in-loop.sh
+++ scripts/run-in-loop.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 IMAGE_TAG=${1:?"Missing IMAGE_TAG"}
 RUNNER_ID=${2:?"Missing RUNNER_ID"}
 
+DOCKER_IMAGE="cpp-kt/gh-runner-ubuntu:$IMAGE_TAG"
+LABELS="$IMAGE_TAG,$(docker inspect --format '{{ index .Config.Labels "gh.labels"}}' "$DOCKER_IMAGE")"
+
 while true; do
     echo "Starting Docker"
     if [[ -f .kill-docker ]]; then
@@ -15,7 +18,7 @@ while true; do
     docker run --rm --name="$RUNNER_ID" \
         --runtime sysbox-runc \
         --env "RUNNER_ID=$RUNNER_ID" \
-        --env "RUNNER_LABELS=ubuntu,$IMAGE_TAG" \
+        --env "RUNNER_LABELS=$LABELS" \
         --env-file "$SCRIPT_DIR/.env" \
-        "cpp-kt/gh-runner-ubuntu:$IMAGE_TAG"
+        "$DOCKER_IMAGE"
 done
