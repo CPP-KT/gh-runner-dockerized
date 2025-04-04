ecfc09b13024fa91fec8565469ec8b98ff5102d0
ivan medvedev <vmedvedev1017@gmail.com>
2025-04-05 01:04:01 +0300

    Separate experimental runner from base

--- experimental/build.sh
+++ experimental/build.sh
@@ -3,9 +3,11 @@ set -eu
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-CPP_COMPILER=${1:?"Missing CPP_COMPILER"}
+RUNNER_VERSION=${1:?"Missing RUNNER_VERSION"}
+CPP_COMPILER=${2:?"Missing CPP_COMPILER"}
 
 docker build "$SCRIPT_DIR" \
     --pull \
-    --tag "cpp-kt/gh-runner-ubuntu:${CPP_COMPILER}-exp" \
-    --build-arg "CPP_COMPILER=${CPP_COMPILER}" 
+    --tag "cpp-kt/gh-runner-ubuntu:$CPP_COMPILER-exp" \
+    --build-arg "CPP_COMPILER=$CPP_COMPILER" \
+    --build-arg "RUNNER_VERSION=$RUNNER_VERSION"
